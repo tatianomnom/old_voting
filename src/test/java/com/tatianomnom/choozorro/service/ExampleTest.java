9aa7cbef78b735581658fb8a882f090388e98ca1
tatianomnom <orangium@gmail.com>
2014-08-14 23:24:16 +0400

    Unit test setup

---
+++ src/test/java/com/tatianomnom/choozorro/service/ExampleTest.java
@@ -0,0 +1,12 @@
+package com.tatianomnom.choozorro.service;
+
+import org.testng.annotations.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+
+@Test
+public class ExampleTest {
+    public void testSomething() {
+        assertThat(1).isBetween(0, 2);
+    }
+}
